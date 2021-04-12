9a618345f8471ade96309f102aefe5bb1820d30a
Alexandre01010 <40190044@esmad.ipp.pt>
2021-04-12 17:19:54 +0100

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config(); // read environment variables from .env file
+const express = require('express');
+const cors = require('cors'); // middleware to enable CORS (Cross-Origin Resource Sharing)
+const app = express();
+const port = process.env.PORT || 8080; // if not defined, use port 8080
+const host = process.env.HOST || '192.78.62.1'; // if not defined, localhost
+app.use(cors()); //enable ALL CORS requests (client requests from other domain)
+app.use(express.json()); //enable parsing JSON body data
+// root route -- /api/
+app.get('/', function (req, res) {
+    res.status(200).json({ message: 'TSIW HOME' });
+});
+// routing middleware for resource TUTORIALS
+app.use('/users', require('./routes/tsiw.routes.js'))
+// handle invalid routes
+app.get('*', function (req, res) {
+    res.status(404).json({ message: 'WHAT???' });
+})
+app.listen(port, host, () => console.log(`App listening at http://${host}:${port}/`));
