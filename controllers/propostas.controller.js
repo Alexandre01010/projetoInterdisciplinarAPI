c5767d56fe3f82a4e85d0ce66ac2db148316e204
Alexandre01010 <40190044@esmad.ipp.pt>
2021-04-27 00:16:27 +0100

    commit

--- controllers/propostas.controller.js
+++ controllers/propostas.controller.js
@@ -147,25 +147,23 @@ const { Op } = require("sequelize")
 //         });
 //       });
 //   };
-exports.createComment = (req, res) => {
-  // Save Comment in the database
-  Comment.create({
-      author: req.body.author, text: req.body.text, tutorialId:
-          req.params.tutorialID
-  })
-      .then(data => {
-          res.status(201).json({
-              message: "New Comment created.", location: "/tutorials/" +
-                  req.params.tutorialID + "/comments/" + data.id
-          });
-          
-      })
-      .catch(err => {
-          if (err.name === 'SequelizeValidationError')
-              res.status(400).json({ message: err.errors[0].message });
-          else
-              res.status(500).json({
-                  message: err.message || "Some error occurred while creating the Comment."
-              });
+exports.createProposal = (req, res) => {
+  // Save Tutorial in the database (IF request body data is validated by Sequelize
+  Proposta.create(req.body)
+    .then((data) => {
+      res.status(201).json({
+        message: "New proposal created.",
+        location: "/propostas/" + data.id,
       });
+    })
+    .catch((err) => {
+      // Tutorial model as validation for the title column (not null)
+      if (err.name === "SequelizeValidationError")
+        res.status(400).json({ message: err.errors[0].message });
+      else
+        res.status(500).json({
+          message:
+            err.message || "Some error occurred while creating the Tutorial.",
+        });
+    });
 };
