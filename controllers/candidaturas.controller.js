a0ed10b398c83587064d1033eff47d5a1fe5dd3c
Alexandre01010 <40190044@esmad.ipp.pt>
2021-05-08 00:14:54 +0100

    commit

--- controllers/candidaturas.controller.js
+++ controllers/candidaturas.controller.js
@@ -17,37 +17,6 @@ exports.getCandidaturas = (req, res) => {
     });
 }
 
-// exports.createCandidatura = (req, res) => {
-//   Proposta.findByPk(req.params.proposalID)
-//     .then(prop => {
-//       if (prop === null) {
-//         res.status(404).json({
-//           message: "Não foi encontrado uma porposta com id " + req.params.proposalID + "."
-//         })
-//       } else {
-//         Candidaturas.create({
-//           id_user: req.body.id_user, id_proposta: req.params.proposalID, mensagem: req.body.mensagem,
-//           id_tipo_estado: req.body.id_tipo_estado, n_ordem_escolha: req.body.n_ordem_escolha
-//         })
-//           .then((data) => {
-//             res.status(201).json({
-//               message: "Candidatura criada com sucesso na proposta com id " + req.params.proposalID + "."
-//             })
-//           })
-//           .catch((err) => {
-//             if (err.name === "SequelizeValidationError") {
-//               res.status(400).json({ message: err.errors[0].message })
-//             } else {
-//               res.status(500).json({
-//                 message:
-//                   err.message || "Some error occurred while creating the Tutorial.",
-//               })
-//             }
-//           })
-//       }
-//     })
-// }
-
 exports.createCandidatura = (req, res) => {
   Proposta.findByPk(req.params.proposalID)
     .then(prop => {
@@ -90,30 +59,6 @@ exports.createCandidatura = (req, res) => {
       }
     })
 }
-
-// exports.createCandidatura = async (req, res) =>{
-//   try{
-//     let pedidoProposta = await Proposta.findByPk(req.params.proposalID)
-//     if(pedidoProposta === null){
-//       res.status(404).json({
-//         message: "Not found proposal with id " + req.params.proposalID
-//       })
-//       return
-//     }
-//     req.body.id_proposta = req.params.proposalID 
-//     console.log(req.body)
-//     let cand = await Candidaturas.create(req.body)
-//     //await Proposta.addCandidaturas(cand)
-
-//     res.status(201).json({ message: "Candidatura criada"})
-
-//   }catch(err){
-//     res.status(500).json({
-//       message: err || "Error creating candidatura on proposal with id " + req.params.proposalID
-//     })
-//   }
-
-// }
 
 exports.getOneCandidatura = (req, res) =>{
   Proposta.findByPk(req.params.proposalID)
