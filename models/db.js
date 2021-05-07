50e23ad86d486f2369ffb9486aef5d206310f088
Alexandre01010 <40190044@esmad.ipp.pt>
2021-05-08 00:27:33 +0100

    commit

--- models/db.js
+++ models/db.js
@@ -26,28 +26,10 @@ db.forum = require("./foruns.model.js")(sequelize, DataTypes);
 //db.foruns.belongsTo(db.propostas);
 db.candidatura = require("./candidaturas.model.js")(sequelize, DataTypes);
 db.tema = require("./temas.model.js")(sequelize, DataTypes);
-//define the 1:N relationship
-//db.proposta.hasMany(db.candidatura); // tutorialId is added into Comment model as FK
-//db.candidatura.belongsTo(db.proposta);
-//db.foruns.belongsTo(db.user)
 
-//entre propostas e usaers belongtoMany
-//foreign key here
-
-
+//relation n:m between user and proposal, that results on candidaturas table
 db.user.belongsToMany(db.proposta, { through: db.candidatura, foreignKey: 'id_user', otherKey: 'id_proposta'})
 db.proposta.belongsToMany(db.user, { through: db.candidatura, foreignKey: 'id_proposta', otherKey: 'id_user'})
-
-
-
-// db.user.belongsToMany(db.proposta, { through: db.candidatura})
-// db.proposta.belongsToMany(db.user, { through: db.candidatura})
-
-
-
-
-// db.candidatura.hasOne(db.proposta)
-// db.proposta.hasMany(db.candidatura)
 
 //entrevitas relations commenting this inorder to test just the entrevistas
 db.participante = require("./participantes.model.js")(sequelize, DataTypes);
