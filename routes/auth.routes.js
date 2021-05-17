103c8517a7f6c0be736b3925b9e63ff950415d3d
Alexandre01010 <40190044@esmad.ipp.pt>
2021-05-17 16:56:53 +0100

    commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authController = require("../controllers/auth.controller");
+let router = express.Router();
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, Content-Type, Accept");
+    next()
+})
+router.route('/signup')
+    .post(authController.signup);
+router.route('/signin')
+    .post(authController.signin)
+router.all('*', function (req, res) {
+    res.status(404).json({ message: 'AUTHENTICATION: what???' });
+})
+module.exports = router;
