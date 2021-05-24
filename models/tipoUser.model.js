a2dbf5ac4536eb9d0187192c1dac0804cb496c40
Alexandre01010 <40190044@esmad.ipp.pt>
2021-05-24 17:49:18 +0100

    commit

--- models/tipoUser.model.js
+++ models/tipoUser.model.js
@@ -1,9 +1,6 @@
 module.exports = (sequelize, DataTypes) =>{
-<<<<<<< HEAD
     const userType = sequelize.define("tipo_user",{
-=======
-    const userType = sequelize.define("tipo user",{
->>>>>>> 16f6943781429d5e48d10c62ebdc715937ac1c49
+
         id_tipo_user:{
             type:DataTypes.INTEGER,
             primaryKey: true
