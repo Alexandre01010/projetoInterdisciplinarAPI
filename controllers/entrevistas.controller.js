31b9849d0be818e6c489cd288e337652ad4bb2e8
Alexandre01010 <40190044@esmad.ipp.pt>
2021-06-08 00:05:47 +0100

    commit

--- controllers/entrevistas.controller.js
+++ controllers/entrevistas.controller.js
@@ -105,14 +105,23 @@ exports.findEntrevistaFilterd = (req, res) => {
             if(data.length != 0){
                 for(let i = 0 ; i < data.length; i++){
                     entrevistas.push(data[i].id_agenda)
-    
                 }
             }
             Entrevistas.findAll( { where: { [Op.and]:[{texto_agenda: {[Op.like]: `%${req.query.texto}%` }}, { [Op.or]: [{ id_agenda: { [Op.in]:entrevistas } }, { id_user: req.loggedUserId }] }] } })
             .then((prop) => {
                 if(prop.length != 0 ){
                     res.status(200).json(prop)
+                }else{
+                    res.status(404).json({
+                        message: "Não há entrevistas associadas"                    
+                    })
                 }
+            })
+            .catch((err)=> {
+                res.status(500).json({
+                    message:
+                      err.message || "Ocorreu um erro ao encontrar candidaturas",
+                  });
             })
         })
     }else{
@@ -128,7 +137,17 @@ exports.findEntrevistaFilterd = (req, res) => {
             .then((prop) => {
                 if(prop.length != 0 ){
                     res.status(200).json(prop)
+                }else{
+                    res.status(404).json({
+                        message: "Não há entrevistas associadas"
+                    })
                 }
+            })
+            .catch((err)=> {
+                res.status(500).json({
+                    message:
+                      err.message || "Ocorreu um erro ao encontrar candidaturas",
+                  });
             })
         })
     }
