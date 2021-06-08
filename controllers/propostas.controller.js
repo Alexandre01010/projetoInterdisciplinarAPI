78518134d6bb6808443705108d9885eee7971a04
Alexandre01010 <40190044@esmad.ipp.pt>
2021-06-08 02:38:03 +0100

    commit

--- controllers/propostas.controller.js
+++ controllers/propostas.controller.js
@@ -28,11 +28,9 @@ exports.create = (req, res) => {
       if (err.name === 'SequelizeValidationError')
         res.status(400).json({ message: err.errors[0].message });
       else {
-        
-          res.status(500).json({
-            message: err || "Ocorreu um erro ao criar uma candidatura à proposta " + req.params.proposalID
-          })
-      
+        res.status(500).json({
+          message: err || "Ocorreu um erro ao criar uma candidatura à proposta " + req.params.proposalID
+        })
       }
     });
 };
