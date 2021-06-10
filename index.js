9972c34e79473d35c1be28940163547be9773ca3
Alexandre01010 <40190044@esmad.ipp.pt>
2021-06-10 19:52:39 +0100

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config(); // read environment variables from .env file
 const express = require('express');
 const cors = require('cors'); // middleware to enable CORS (Cross-Origin Resource Sharing)
 const app = express();
-const port = process.env.PORT || 3000; // if not defined, use port 8080
+const port = process.env.PORT; // if not defined, use port 8080
 app.use(cors()); //enable ALL CORS requests (client requests from other domain)
 app.use(express.json()); //enable parsing JSON body data
 // root route -- /api/
